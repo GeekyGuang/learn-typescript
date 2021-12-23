35738e53a890db5557ce72b66700c231424ba09f
TravisWongX <travis.x.wong@gmail.com>
2021-12-23 17:36:03 +0800

    any和union

--- basicType.ts
+++ basicType.ts
@@ -7,3 +7,11 @@ let firstName: string = 'hello'
 
 let u: undefined = undefined
 let n: null = null
+
+let notSure: any = 4
+notSure = true
+notSure.myName
+
+// 联合类型
+let numberOrString: number | string = 100
+numberOrString = 'hello'
