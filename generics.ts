ec713bc53b7ac37e75d9428b3a8e3c11e6920dfb
TravisWongX <travis.x.wong@gmail.com>
2021-12-26 20:09:00 +0800

    泛型generics

--- generics.ts
+++ generics.ts
@@ -27,3 +27,46 @@ function echoWithArray<T extends IWithLength>(arg: T): T {
 echoWithArray([1, 2, 3])
 echoWithArray({ length: 10 })
 echoWithArray('hello')
+
+class Queue<T> {
+  private data = []
+  push(item: T) {
+    return this.data.push(item)
+  }
+  pop(): T {
+    return this.data.shift()
+  }
+}
+
+const queue = new Queue<number>()
+queue.push(1)
+console.log(queue.pop().toFixed())
+
+const queue2 = new Queue<string>()
+queue2.push('str')
+console.log(queue2.pop().length)
+
+interface KeyPair<T, U> {
+  key: T
+  value: U
+}
+
+let kp1: KeyPair<number, string> = { key: 1, value: 'str' }
+let kp2: KeyPair<string, number> = { key: 'str', value: 123 }
+
+let arr: Array<number> = [1, 2, 3]
+
+interface IPlus<T> {
+  (a: T, b: T): T
+}
+
+function plus(a: number, b: number): number {
+  return a + b
+}
+
+function connect(a: string, b: string): string {
+  return a + b
+}
+
+const a: IPlus<number> = plus
+const b: IPlus<string> = connect
